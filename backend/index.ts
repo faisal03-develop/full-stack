099083147a6e5c9e98b2f6fc80ecd8959c509445
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-05 18:30:06 +0500

    Added the Dummy Data from the seed file.

--- backend/index.ts
+++ backend/index.ts
@@ -1,48 +1,13 @@
-import { prisma } from './lib/prisma'
+import express from "express";
 
-async function main() {
-//   const user = await prisma.user.create({
-//     data: {
-//       name: 'Alice',
-//       email: 'alice@prisma.io',
-//     },
-//   })
-//   console.log(user)
+const app = express();
 
-//   const user = await prisma.user.create({
-//     data: {
-//       name: 'Bob',
-//       email: 'bob@prisma.io',
-//       posts: {
-//         create: [
-//           {
-//             title: 'Hello World',
-//             published: true
-//           },
-//           {
-//             title: 'My second post',
-//             content: 'This is still a draft'
-//           }
-//         ],
-//       },
-//     },
-//   })
-//   console.log(user)
 
- const usersWithPosts = await prisma.user.findMany({
-    include: {
-      posts: true,
-    },
-  })
-  console.dir(usersWithPosts, { depth: null })
-}
 
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+}); 
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,6 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from '@prisma/adapter-pg';
+import "dotenv/config";
 
-const prisma = new PrismaClient({});
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 async function seed() {
     const user = await prisma.user.create({
@@ -16,4 +20,4 @@ async function seed() {
     })
 }
 
-seed()
+seed() .then(() => prisma.$disconnect);
