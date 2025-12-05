62c5fbfb7d3587acdadb116afb279a055a984be9
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-05 11:13:56 +0500

    Added the Cors, Axios, and Connected the BackEnd With the Front end

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,35 @@
 import express from 'express';
+import cors from 'cors';
 
 
+
+app.use(cors());
 const PORT = process.env.PORT;
 const app = express();
+
+const jokes = [
+    {
+        id:1,
+        title:'title 1',
+        content:'content 1',
+    },
+    {
+        id:2,
+        title:'title 2',
+        content:'content 2',
+    },
+]
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+app.get('/jokes', (req, res) => {
+    res.json(jokes);
+});
+
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
