0162ef86477b1956bb96111c0f09f2720ec3f1f6
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-08 14:11:49 +0500

    Relation Established

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,35 +1,11 @@
-"use client";
 
-import { useEffect, useState } from "react";
-import axios from "axios";
-
+import Header from "@/components/header";
 
 export default function Home() {
-const [jokes,setJokes] = useState([]);
-
-useEffect(() => {
-  axios.get("http://localhost:5000/api/jokes")
-  .then((response) => {
-      setJokes(response.data);
-    }
-    ).catch((error) => {
-      console.log(error);
-    }
-    );
-  })
 
   return (
     <div>
-      <h1>Home</h1>
-      <p>Jokes: {jokes.length}</p>
-
-      {
-      jokes.map((joke ,index) => (
-        <div key={joke.id}>
-          <h3>{joke.title}</h3>
-          <p>{joke.content}</p>
-        </div>
-      ))}
+      <Header />
     </div>
   );
 }
