856b46e455759368a181beb2aeb4e301b83d5e0e
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-09 10:53:41 +0500

    Added the generic Treatment Plan With Props

--- frontend/components/plan.tsx
+++ frontend/components/plan.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
 
-export default function plan() {
+interface PlanProps {
+  heading: string;
+  points: string[];
+  price: string;
+}
+
+export default function plan({ heading, points, price }: PlanProps) {
   return (
     <div>
-        <div>
-            <p>Heading</p>
-            <p>point 1</p>
-            <p>point 2</p>
-            <p>point 3</p>
-            <p>point 4</p>
-            <button className="bg-(--purple) h-10 w-30 rounded-2xl text-white ml-6">Get Started</button>
+        <div className='bg-white w-124 p-8 mt-20'>
+          <div>
+            <p className='text-5xl mb-4 text-blue-900'>{heading}</p>
+            <ul>
+              {points.map((point, index) => (
+                <li key={index} className='my-6 mx-4 text-blue-900'>{point}</li>
+              ))}
+            </ul>
+          </div>
+            <div className='flex flex-row gap-6 items-center'>
+              <div className='border-2 border-purple-600 rounded-4xl h-10 w-30 flex justify-center items-center text-purple-600'>{price}</div>
+              <button className="bg-purple-600 h-10 w-30 rounded-4xl text-white ml-6">Get Started</button>
+            </div>
         </div>
     </div>
   )
