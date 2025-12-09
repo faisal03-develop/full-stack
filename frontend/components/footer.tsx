9d70cb462b0cf13ce6d690723b7290b33f5956e6
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-09 12:48:39 +0500

    Added the address area

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -3,18 +3,31 @@ import Image from "next/image"
 
 export default function footer() {
   return (
-    <div className="mt-20 bg-(--gray) p-20">
-      <div className="flex flex-row w-160 gap-7">
-        <Image src="/images/logo.png" alt="logo" height={100} width={100}></Image>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure aspernatur fugit dolores ipsum quam laborum dolore, est explicabo doloribus nemo. Rem tenetur expedita, ad beatae maxime eligendi. Fugiat, ratione voluptatem.</p>
+    <div className="mt-20 bg-(--gray) p-20 flex flex-row justify-between">
+      <div className="flex flex-row items-center w-160 gap-7">
+        <Image src="/images/logo.png" alt="logo" height={100} width={140}></Image>
+        <p className="text-(--darkblue)">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure aspernatur fugit dolores ipsum quam laborum dolore, est explicabo doloribus nemo. Rem tenetur expedita, ad beatae maxime eligendi. Fugiat, ratione voluptatem.</p>
       </div>
       <div>
-        <div>
-          <h1 className="text-2xl font-bold">Contact Us</h1>
+        <div className="flex flex-col items-start">
+          <span className="text-2xl text-(--purple) font-bold">Contact Us</span>
+          <div className="flex flex-row gap-5 my-4" >
           <Image src="/images/call.png" alt="freepik phone" height={30} width={30}></Image>
           <p>+92-312-3456789</p>
+          </div>
+          <div  className="flex flex-row gap-5 my-4" >
           <Image src="/images/email.png" alt="freepik email" height={30} width={30}></Image>
           <p>MediCare@gmail.com</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="flex flex-col items-start">
+          <span className="text-2xl text-(--purple) font-bold">Address</span>
+          <div className="flex flex-row gap-5 my-4" >
+          <Image src="/images/location.png" alt="freepik location" height={30} width={30}></Image>
+          <p>MediCare, Karachi, Pakistan</p>
+          </div>
         </div>
       </div>
     </div>
